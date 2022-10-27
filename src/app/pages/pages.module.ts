75dbbb57f3f437f966b0bd282226a96c4716f25b
Alexson Felipe <alexson.silveira@triersistemas.com.br>
2022-10-26 21:52:08 -0300

    First commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing.module';
+import { DetailsComponent } from './details/details.component';
+
+@NgModule({
+  declarations: [HomeComponent, DetailsComponent],
+  imports: [CommonModule, RoutingModule],
+})
+export class PagesModule {}
